f354c65ccbfe44c3db745d1125aae45dffc066de
jithin <jithinajith11@gmail.com>
2018-09-27 12:52:32 +0530

    successfull button

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+let name;
+let id;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('form');
 });
 
-router.post('/print-data', function(req, res, next) {
+router.post('/', function(req, res, next) {
 	//var details= req.body;
-	 var name= req.body.name;
-		var id= req.body.emailid;
+	  name= req.body.name;
+	 id= req.body.emailid;
 	res.render('print-data',{name,id});
-		fs.appendFile('./file/details.txt',`name: ${name}	emailid: ${id}`+ '\r\n' , function(err) {
+});
+
+router.post('/success',function(req,res,next) {
+	fs.appendFile('./file/details.txt',`name: ${name}	emailid: ${id}`+ '\r\n' , function(err) {
 	if (err) throw err;
-	console.log('successfull updated!!');
+	res.writeHead(200,{'content-type': 'text/html'});
+	res.write("successfully updated!! :)");
+	res.write('<form action="/" method="get">');
+	res.write('<button> new</button>');
+	res.write('</form>');
+
 	//next(details);
 });
 });
